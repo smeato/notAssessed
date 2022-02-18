4b1d4cf60757100f5bf0fcf4eae8be3418ac2a78
Anna Smeaton <2263320s@student.gla.ac.uk>
2022-02-18 15:23:05 +0000

    chapter 10 passing hopefully database gone

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,7 @@ from rango.forms import PageForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required 
+from datetime import datetime 
 
 
 def index(request):
@@ -19,11 +20,18 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+
+    visitor_cookie_handler(request) 
+    
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Anna'}
-    return render(request, 'rango/about.html', context=context_dict)
+
+    visitor_cookie_handler(request) 
+    context_dict['visits'] = request.session['visits'] 
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response 
 
 
 def show_category(request, category_name_slug):
@@ -153,3 +161,24 @@ def user_logout(request):
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html') 
+
+
+def visitor_cookie_handler(request): 
+    visits = int(get_server_side_cookie(request, 'visits', '1')) 
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now())) 
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S') 
+
+    if (datetime.now() - last_visit_time).days >0: 
+        visits = visits + 1 
+        request.session['last_visit'] = str(datetime.now()) 
+    else: 
+        request.session['last_visit'] = last_visit_cookie 
+    
+    request.session['visits'] = visits 
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie) 
+    if not val: 
+        val = default_val 
+    return val 
+
